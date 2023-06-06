b3cdb567ebcd4b50d236f1068d563afe487fcfb9
Dávid Mikuš (Dasio) <david.mikus@kiwi.com>
2023-06-06 16:28:29 +0200

    feat(cache): SA service accoutns + vault tokens

--- pkg/vault/auth.go
+++ pkg/vault/auth.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"sync"
+	"time"
 
 	vaultApi "github.com/hashicorp/vault/api"
 	authenticationv1 "k8s.io/api/authentication/v1"
@@ -28,18 +30,21 @@ func (a AuthToken) Token() (string, error) {
 }
 
 type AuthServiceAccount struct {
-	name        string
-	namespace   string
-	role        string
-	path        string
-	vaultClient *vaultApi.Client
-	autoMount   bool
-	k8ClientSet *kubernetes.Clientset
+	name             string
+	namespace        string
+	role             string
+	path             string
+	vaultClient      *vaultApi.Client
+	autoMount        bool
+	k8ClientSet      *kubernetes.Clientset
+	cacheMx          sync.RWMutex
+	cachedVaultToken string
+	vaultTokenExpire time.Time
 }
 
 func NewAuthServiceAccount(vaultClient *vaultApi.Client, k8ClientSet *kubernetes.Clientset,
-	name, namespace, role, path string, automount bool) AuthServiceAccount {
-	return AuthServiceAccount{
+	name, namespace, role, path string, automount bool) *AuthServiceAccount {
+	return &AuthServiceAccount{
 		name:        name,
 		namespace:   namespace,
 		role:        role,
@@ -49,8 +54,18 @@ func NewAuthServiceAccount(vaultClient *vaultApi.Client, k8ClientSet *kubernetes
 		k8ClientSet: k8ClientSet,
 	}
 }
+func (a *AuthServiceAccount) cachedToken() string {
+	a.cacheMx.Lock()
+	defer a.cacheMx.Unlock()
+	return a.cachedVaultToken
+}
 
-func (a AuthServiceAccount) Token() (string, error) {
+func (a *AuthServiceAccount) Token() (string, error) {
+	vaultToken := a.cachedToken()
+	if vaultToken != "" && time.Now().Add(30*time.Second).Before(a.vaultTokenExpire) {
+		return vaultToken, nil
+	}
+
 	jwtToken, err := a.fetchJWT()
 	if err != nil {
 		return "", fmt.Errorf("could not fetch JWT token: %w", err)
@@ -61,6 +76,7 @@ func (a AuthServiceAccount) Token() (string, error) {
 		"jwt":  jwtToken,
 	}
 
+	t := time.Now()
 	resp, err := a.vaultClient.Logical().Write(a.path, data)
 	if err != nil {
 		return "", fmt.Errorf("failed to login to Vault with JWT: %w", err)
@@ -71,14 +87,21 @@ func (a AuthServiceAccount) Token() (string, error) {
 		return "", fmt.Errorf("could not read auth token from response: %w", err)
 	}
 
-	// resp has resp.LeaseDuration and resp.Tokener.LeaseDuration which can cause confusion
-	// with autocomplete, use TokenTTL method instead
-	// ttl, err := resp.TokenTTL()
+	duration, err := resp.TokenTTL()
+	if err != nil {
+		return "", fmt.Errorf("could not read auth token TTL from response: %w", err)
+	}
+	expireIn := t.Add(duration)
+
+	a.cacheMx.Lock()
+	defer a.cacheMx.Unlock()
+	a.cachedVaultToken = token
+	a.vaultTokenExpire = expireIn
 
 	return token, nil
 }
 
-func (a AuthServiceAccount) fetchJWT() (string, error) {
+func (a *AuthServiceAccount) fetchJWT() (string, error) {
 	if a.autoMount {
 		return fetchJWTFromAutoMountedSecret()
 	}
